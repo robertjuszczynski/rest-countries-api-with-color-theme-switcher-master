ec0fe4e1c014334ef433d99ccb17087b80373248
Robert Juszczyński <robertjuszczynski@o2.pl>
2023-04-07 18:26:33 +0200

    Styles
    Some Tailwind styles, Axios Imported

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkmode: 'class',
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+       colors: {
+        'Dark-Blue-Dark-Mode-Elements': 'hsl(209, 23%, 22%)',
+        'Very-Dark-Blue-Dark-Mode-Background': 'hsl(207, 26%, 17%)',
+        'Very-Dark-Blue-Light-Mode-Text': 'hsl(200, 15%, 8%)',
+        'Dark-Gray-Light-Mode-Input': 'hsl(0, 0%, 52%)',
+        'Very-Light-Gray-Light-Mode-Background': 'hsl(0, 0%, 98%)',
+        'White-Dark-Mode-Text-&-Light-Mode-Elements': 'hsl(0, 0%, 100%)',
+      },
+      maxWidth: {
+        'select': '250px',
+      },
+      width: {
+        '512': '512px',
+        '250': '250px'
+      },
+      height: {
+        '60': '60px',
+      }
+    },
   },
   plugins: [],
 }
