6cb4b992e66cde3d70c5f49ba31b407452c33bc9
Robert Juszczyński <robertjuszczynski@o2.pl>
2023-04-11 17:56:25 +0200

    Added filter by region function

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,19 @@
-const countriesWrapper = document.querySelector("#CountryWrapper");
-const searchInput = document.querySelector('input[type="search"]');
+const countriesWrapper = document.querySelector("#country-wrapper");
+const filtersContainer = document.querySelector("#filters-container")
+const searchInput = document.querySelector('#country-search');
 const regionFilterBtn = document.querySelector("#region-filter-btn");
 const regionMenu = regionFilterBtn.querySelector("ul");
+const Regions = regionMenu.querySelectorAll("li");
+const urlParams = new URLSearchParams(window.location.search);
 
 const getAllData = async () => {
   try {
     const allData = await axios.get("https://restcountries.com/v3.1/all");
+    if (urlParams.has("reg")) {
+      regionFilterBtn.children[0].innerText = urlParams.get("reg");
+      searchCountriesByRegion(urlParams.get("reg"))
+      return
+    }
     displayCountries(allData.data);
   } catch (error) {
     countriesWrapper.textContent = "Can't get any Countries!";
@@ -34,7 +42,7 @@ const searchCountriesByRegion = async (region) => {
     countriesWrapper.textContent = "No countries found!";
     console.log(error);
   }
-}
+};
 const displayCountries = (countries) => {
   countriesWrapper.innerHTML = "";
   countries.forEach((country) => {
@@ -43,7 +51,7 @@ const displayCountries = (countries) => {
         country.flags.png
       }')"></div>
       <div class="h-[52.5%] px-6 leading-[1.7]">
-        <p class="font-extrabold text-base mt-5 mb-4">${
+        <p data-country-name="${country.name.official}" class="font-extrabold text-base mt-5 mb-4">${
           country.name.official
         }</p>
         <p><span class="font-semibold">Population:</span> ${country.population.toLocaleString(
@@ -55,6 +63,10 @@ const displayCountries = (countries) => {
     </div>`;
     countriesWrapper.insertAdjacentHTML("beforeend", countryElement);
   });
+};
+
+const viewCountryDetails = (country) => {
+
 };
 
 let timeoutId;
@@ -74,5 +86,16 @@ regionFilterBtn.addEventListener("click", () => {
   regionFilterBtn.children[1].classList.toggle("rotate-180");
   regionMenu.classList.toggle("hidden");
   regionMenu.classList.toggle("animate-fade");
+});
+
+Regions.forEach((Region) => {
+  Region.addEventListener("click", () => {
+    if (!(regionFilterBtn.children[0].innerText == Region.innerText)) {
+      regionFilterBtn.children[0].innerText = Region.innerText;
+      urlParams.set("reg", `${Region.innerText}`)
+      window.location.search = urlParams;
+      return
+    }
+  });
 });
 getAllData();
