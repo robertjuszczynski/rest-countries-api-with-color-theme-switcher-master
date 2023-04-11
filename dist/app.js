834c7d460b62489fb97c70d0908dec5bd6f40f78
Robert Juszczyński <robertjuszczynski@o2.pl>
2023-04-11 21:28:26 +0200

    Improved performance and readability
    Changed two request functions to two arrow functions without API requests, code readability improved, code shortened

--- dist/app.js
+++ dist/app.js
@@ -3,55 +3,52 @@ const filtersContainer = document.querySelector("#filters-container")
 const searchInput = document.querySelector('#country-search');
 const regionFilterBtn = document.querySelector("#region-filter-btn");
 const regionMenu = regionFilterBtn.querySelector("ul");
-const Regions = regionMenu.querySelectorAll("li");
+const regions = regionMenu.querySelectorAll("li");
 const urlParams = new URLSearchParams(window.location.search);
+
+let allCountriesData = [];
 
 const getAllData = async () => {
   try {
     const allData = await axios.get("https://restcountries.com/v3.1/all");
+    allCountriesData = allData.data;
+    console.log(allCountriesData)
+    displayCountries(allCountriesData);
     if (urlParams.has("reg")) {
       regionFilterBtn.children[0].innerText = urlParams.get("reg");
       searchCountriesByRegion(urlParams.get("reg"))
       return
     }
-    displayCountries(allData.data);
   } catch (error) {
     countriesWrapper.textContent = "Can't get any Countries!";
     console.log(error);
   }
 };
 
-const searchCountriesByName = async (name) => {
-  try {
-    const searchedCountries = await axios.get(
-      `https://restcountries.com/v3.1/name/${name}`
-    );
-    displayCountries(searchedCountries.data);
-  } catch (error) {
-    countriesWrapper.textContent = "No countries found!";
-    console.log(error);
-  }
+const searchCountriesByName = (name) => {
+  const searchedCountries = allCountriesData.filter((country) =>
+    country.name.official.toLowerCase().includes(name.toLowerCase())
+  );
+  searchedCountries.length === 0 ? countriesWrapper.textContent = "No countries found!" : displayCountries(searchedCountries);
 };
-const searchCountriesByRegion = async (region) => {
-  try {
-    const searchedCountries = await axios.get(
-      `https://restcountries.com/v3.1/region/${region}`
-    );
-    displayCountries(searchedCountries.data);
-  } catch (error) {
-    countriesWrapper.textContent = "No countries found!";
-    console.log(error);
-  }
+
+const searchCountriesByRegion = (region) => {
+  const searchedCountries = allCountriesData.filter((country) =>
+    country.region.toLowerCase().includes(region.toLowerCase())
+  );
+  console.log(searchedCountries)
+  searchedCountries.length === 0 ? countriesWrapper.textContent = "No countries found!" : displayCountries(searchedCountries);
 };
+
 const displayCountries = (countries) => {
-  countriesWrapper.innerHTML = "";
+  countriesWrapper.innerHTML && (countriesWrapper.innerHTML = "");
   countries.forEach((country) => {
     const countryElement = `<div class="country-container h-80 bg-White-Dark-Mode-Text-&-Light-Mode-Elements overflow-hidden rounded-md shadow-md hover:scale-110 focus:scale-110 ease-out duration-300">
       <div class="bg-cover bg-center bg-no-repeat h-[47.5%]" style="background-image: url('${
         country.flags.png
       }')"></div>
       <div class="h-[52.5%] px-6 leading-[1.7]">
-        <p data-country-name="${country.name.official}" class="font-extrabold text-base mt-5 mb-4">${
+        <p class="font-extrabold text-base mt-5 mb-4">${
           country.name.official
         }</p>
         <p><span class="font-semibold">Population:</span> ${country.population.toLocaleString(
@@ -62,24 +59,18 @@ const displayCountries = (countries) => {
       </div>
     </div>`;
     countriesWrapper.insertAdjacentHTML("beforeend", countryElement);
-  });
+  })
 };
 
-const viewCountryDetails = (country) => {
-
+const showCountryDetails = (country) => {
+  console.log(country)
+  filtersContainer.classList.toggle('hidden')
+  countriesWrapper.classList.toggle('hidden')
 };
 
-let timeoutId;
 searchInput.addEventListener("input", () => {
   const inputValue = searchInput.value;
-  clearTimeout(timeoutId);
-  if (inputValue !== "") {
-    timeoutId = setTimeout(() => {
-      searchCountriesByName(inputValue);
-    }, 200);
-  } else {
-    getAllData();
-  }
+  inputValue !== "" ? searchCountriesByName(inputValue) : displayCountries(allCountriesData);
 });
 
 regionFilterBtn.addEventListener("click", () => {
@@ -88,14 +79,16 @@ regionFilterBtn.addEventListener("click", () => {
   regionMenu.classList.toggle("animate-fade");
 });
 
-Regions.forEach((Region) => {
-  Region.addEventListener("click", () => {
-    if (!(regionFilterBtn.children[0].innerText == Region.innerText)) {
-      regionFilterBtn.children[0].innerText = Region.innerText;
-      urlParams.set("reg", `${Region.innerText}`)
-      window.location.search = urlParams;
-      return
+regions.forEach((region) => {
+  region.addEventListener("click", () => {
+    let selectedRegion = region.innerText;
+    if (regionFilterBtn.children[0].innerText !== selectedRegion) {
+      regionFilterBtn.children[0].innerText = selectedRegion;
+      urlParams.set("reg", selectedRegion);
+      window.history.replaceState({}, '', `${location.pathname}?${urlParams}`);
+      searchCountriesByRegion(selectedRegion);
     }
   });
 });
+
 getAllData();
